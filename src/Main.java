b5e1d046ea5cdf378bf8fc0c04c6f3d9c629c470
Hades1123 <dp1.1a7.si@gmail.com>
2025-02-22 21:09:11 +0700

    calculator first

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println(Calculator.add(1, 2));
         System.out.println(Calculator.subtract(1, 2));
+        System.out.println(Calculator.multiply(122, 200));
+        System.out.println(Calculator.divide(122, 10));
     }
 }
